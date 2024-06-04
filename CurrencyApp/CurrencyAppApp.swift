715c01fb06594a98a4820900f66e4b8f8d8851cf
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-06-04 12:47:08 +0100

    Initial Commit

---
+++ CurrencyApp/CurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyAppApp.swift
+//  CurrencyApp
+//
+//  Created by Micko, Sebastian (AMM) on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
